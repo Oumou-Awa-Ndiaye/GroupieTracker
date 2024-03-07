958eb56f65a9b01906d63e03b6387a29ead0390f
noumouaw <oumouawa.ndiaye@ynovcom>
2024-03-07 09:47:29 +0100

    lllll

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -30,6 +31,9 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("MUUUSIICCC")
 
+	searchEntry := widget.NewEntry()
+	searchEntry.SetPlaceHolder("Recherche...")
+
 	// Exemple de données
 	var bands Bands
 	err := json.Unmarshal([]byte(sampleData), &bands)
@@ -37,89 +41,60 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Exemple d'utilisation des fonctions de filtrage
-	minCreation := 2000
-	maxCreation := 2010
-	minMembers := 3
-	maxMembers := 5
-	concertLocations := []string{"New York", "Los Angeles"}
+	// Fonction de mise à jour des résultats en fonction de la recherche
+	updateResults := func(query string) {
+		filteredResults := filterBySearchQuery(bands, query)
+		fmt.Println("Résultats de la recherche :", filteredResults)
+		// Mettez à jour l'interface utilisateur avec les résultats de la recherche ici
+	}
 
-	filteredByCreationDate := filterByCreationDate(bands, minCreation, maxCreation)
-	fmt.Println("Groupes filtrés par date de création :", filteredByCreationDate)
+	// Réagir aux changements dans la barre de recherche
+	searchEntry.OnChanged = func(query string) {
+		updateResults(query)
+	}
 
-	filteredByMembersCount := filterByMembersCount(bands, minMembers, maxMembers)
-	fmt.Println("Groupes filtrés par nombre de membres :", filteredByMembersCount)
-
-	filteredByConcertLocations := filterByConcertLocations(bands, concertLocations)
-	fmt.Println("Groupes filtrés par lieux de concerts :", filteredByConcertLocations)
-
-	w.SetContent(widget.NewLabel("MUUUUSICCCC!"))
+	w.SetContent(container.NewVBox(searchEntry, widget.NewLabel("MUUUUSICCCC!")))
 	w.ShowAndRun()
 }
 
-// Fonction pour filtrer les groupes de musique par date de création
-func filterByCreationDate(bands Bands, minCreationDate, maxCreationDate int) []Artist {
-	var filteredBands []Artist
+// Fonction pour filtrer les résultats en fonction de la requête de recherche
+func filterBySearchQuery(bands Bands, query string) []Artist {
+	var filteredResults []Artist
 	for _, band := range bands.Index {
-		if band.CreationDate >= minCreationDate && band.CreationDate <= maxCreationDate {
-			filteredBands = append(filteredBands, band)
+		// Vérifier si le nom du groupe contient la requête de recherche (insensible à la casse)
+		if strings.Contains(strings.ToLower(band.Name), strings.ToLower(query)) {
+			filteredResults = append(filteredResults, band)
 		}
 	}
-	return filteredBands
-}
-
-// Fonction pour filtrer les groupes de musique par nombre de membres
-func filterByMembersCount(bands Bands, minMembers, maxMembers int) []Artist {
-	var filteredBands []Artist
-	for _, band := range bands.Index {
-		numMembers := len(band.Members)
-		if numMembers >= minMembers && numMembers <= maxMembers {
-			filteredBands = append(filteredBands, band)
-		}
-	}
-	return filteredBands
-}
-
-// Fonction pour filtrer les groupes de musique par lieux de concerts
-func filterByConcertLocations(bands Bands, locations []string) []Artist {
-	var filteredBands []Artist
-	for _, band := range bands.Index {
-		for _, loc := range locations {
-			if strings.Contains(band.Locations, loc) {
-				filteredBands = append(filteredBands, band)
-				break
-			}
-		}
-	}
-	return filteredBands
+	return filteredResults
 }
 
 // Données JSON d'exemple
 const sampleData = `
 {
-	"index": [
-		{
-			"id": 1,
-			"image": "image1.jpg",
-			"name": "Groupe 1",
-			"members": ["Membre 1", "Membre 2"],
-			"creationDate": 2000,
-			"firstAlbum": "Album 1",
-			"locations": "New York, Londres",
-			"concertDates": "2024-02-10, 2024-02-15",
-			"relations": "Relation 1"
-		},
-		{
-			"id": 2,
-			"image": "image2.jpg",
-			"name": "Groupe 2",
-			"members": ["Membre 3", "Membre 4", "Membre 5"],
-			"creationDate": 2010,
-			"firstAlbum": "Album 2",
-			"locations": "Los Angeles",
-			"concertDates": "2024-02-12, 2024-02-17",
-			"relations": "Relation 2"
-		}
-	]
+    "index": [
+        {
+            "id": 1,
+            "image": "image1.jpg",
+            "name": "Groupe 1",
+            "members": ["Membre 1", "Membre 2"],
+            "creationDate": 2000,
+            "firstAlbum": "Album 1",
+            "locations": "New York, Londres",
+            "concertDates": "2024-02-10, 2024-02-15",
+            "relations": "Relation 1"
+        },
+        {
+            "id": 2,
+            "image": "image2.jpg",
+            "name": "Groupe 2",
+            "members": ["Membre 3", "Membre 4", "Membre 5"],
+            "creationDate": 2010,
+            "firstAlbum": "Album 2",
+            "locations": "Los Angeles",
+            "concertDates": "2024-02-12, 2024-02-17",
+            "relations": "Relation 2"
+        }
+    ]
 }
 `
