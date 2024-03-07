797ec0f86fb2dfafc7424581f2cc83fd66b97d95
flisa <lisa.fedoul>
2024-03-07 14:19:55 +0100

    les fichiers de structs ainsi que les fonctions pour la barre de recherche(version initial)

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("MUUUSIICCC")
+	w := a.NewWindow("Groupie_Tracker")
 
-	w.SetContent(widget.NewLabel("MUUUUSICCCC!"))
+	// Créer une barre de recherche
+	searchEntry := widget.NewEntry()
+	searchEntry.SetPlaceHolder("Rechercher...")
+
+	// Créer un widget de liste pour afficher les suggestions
+	suggestionsList := widget.NewList(
+		func() int {
+			return 0 // Initialisation avec une liste vide
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("")
+		},
+		func(i widget.ListItemID, obj fyne.CanvasObject) {
+			obj.(*widget.Label).SetText("") // La fonction de mise à jour sera définie ultérieurement
+		},
+	)
+
+	// Créer un conteneur pour la barre de recherche et la liste de suggestions
+	content := container.NewVBox(
+		searchEntry,
+		suggestionsList,
+	)
+	// Définir le contenu de la fenêtre
+	w.SetContent(content)
+
 	w.ShowAndRun()
 }
