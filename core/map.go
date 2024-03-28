5e383bec80b6080c863d06babac8d41ce7bdefe4
dlou <lou.desumeur@ynov.com>
2024-03-28 15:20:56 +0100

    ld

--- core/map.go
+++ core/map.go
@@ -1,6 +1,6 @@
 package core
 
-import (
+/*import (
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -41,4 +41,36 @@ func Map() {
 
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+}
+*/
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func GetLocation(Artist) {
+	url := "https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=118f1dbf888648258df3f09eb742819a"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
