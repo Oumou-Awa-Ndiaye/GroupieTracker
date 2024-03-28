9adf1b605158424dfc7f69c1feddac20d9bc03a2
flisa <lisa.fedoul>
2024-03-28 11:48:37 +0100

    rajout de la fonctions des suggestions

--- core/structs.go
+++ core/structs.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"encoding/json"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"net/http"
 )
 
@@ -9,6 +11,7 @@ import (
 type Artist struct {
 	Id           int      `json:"id"`
 	Image        string   `json:"image"`
+	ImageURL     string   `json:"imageUrl"`
 	Name         string   `json:"name"`
 	Members      []string `json:"members"`
 	DateCreation int      `json:"creationDate"`
@@ -77,4 +80,12 @@ func GetRelations() []Relation {
 	var relations []Relation
 	json.NewDecoder(resp.Body).Decode(&relations)
 	return relations
+}
+func getImageFromURL(urlStr string) *canvas.Image {
+	resource, _ := fyne.LoadResourceFromURLString(urlStr)
+
+	image := canvas.NewImageFromResource(resource)
+	image.FillMode = canvas.ImageFillContain
+	image.SetMinSize(fyne.NewSize(200, 200)) // Adjust the size as needed
+	return image
 }
