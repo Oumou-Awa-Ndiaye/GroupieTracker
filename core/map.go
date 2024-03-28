b145bee32677225e78fdcefd0fe018f901fdb8f3
dlou <lou.desumeur@ynov.com>
2024-03-28 15:21:39 +0100

    ld

--- core/map.go
+++ core/map.go
@@ -1,48 +1,5 @@
 package core
 
-/*import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
-
-type Location struct {
-	Latitude  float64 `json:"lat"`
-	Longitude float64 `json:"lon"`
-}
-
-func Geocode(address string) (Location, error) {
-	return Location{49.59380, 8.15052}, nil
-}
-
-func Map() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		artist := r.FormValue("artist")
-
-		location, err := Geocode(artist)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		response, err := json.Marshal(location)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(response)
-	})
-
-	fmt.Println("Server running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}
-*/
 import (
 	"fmt"
 	"io/ioutil"
